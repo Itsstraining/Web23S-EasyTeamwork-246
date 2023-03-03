cadd9f62fbc9ba012d9965f1602d83d2a5a1d917
NGuyenHuytri <tri.nh12825@sinhvien.hoasen.edu.vn>
2023-03-03 10:07:55 +0700

    v0.5

--- client/src/app/pages/hometask/hometask.component.ts
+++ client/src/app/pages/hometask/hometask.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./hometask.component.scss']
 })
 export class HometaskComponent {
+  items = ["item1", "item2", "item3"];
+  people = ["James", "Viper", "Phillip"]
 
+  drop(event: CdkDragDrop<string[]>){
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  dropped(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
