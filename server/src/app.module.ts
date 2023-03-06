36bc1ee1d42f9fa9c71e2064aab0ea32b99864a9
Sơn ĐInh Trung <SON.DT13376@sinhvien.hoasen.edu.vn>
2023-03-06 15:00:36 +0700

    aaa

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,8 +7,8 @@ import { UsersModule } from './module/users.module';
 @Module({
   imports: [
     // //tao file .env 
-    //MONGODB_URL=mongodb+srv://user:password@cluster0.fidun.mongodb.net/Hotel?retryWrites=true&w=majority
-    MongooseModule.forRoot(process.env.MONGODB_URL),
+    //MONGODB_URL=mongodb+srv://<username>:<password>@cluster0.za2rizv.mongodb.net/todotask?retryWrites=true&w=majority
+    MongooseModule.forRoot('mongodb+srv://Dts16092003:Dts16092003@cluster0.za2rizv.mongodb.net/todotask?retryWrites=true&w=majority'),
     UsersModule,
   ],
   controllers: [AppController],
