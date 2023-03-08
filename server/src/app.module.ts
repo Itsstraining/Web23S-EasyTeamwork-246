6b4b715d4c6d0806908da9fe68153300a276df73
NGuyenHuytri <tri.nh12825@sinhvien.hoasen.edu.vn>
2023-03-08 09:44:05 +0700

    c1

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './module/users.module';
+import { TaskService } from './services/task/task.service';
+import { TaskController } from './controllers/task/task.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TaskController],
+  providers: [AppService, TaskService],
 })
 export class AppModule { }
--- server/src/module/users.module.ts
+++ server/src/module/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from 'src/controllers/users/users.controller';
 import { User, UserSchema } from 'src/schemas/users.schema';
 import { UsersService } from 'src/services/users/users.service';
+import { TaskModule } from './task.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        TaskModule,
     ],
     controllers: [UsersController],
     providers: [UsersService],
