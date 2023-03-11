87f6bb80ce66a6a9c2f59540bf078b80bdeff093
NGuyenHuytri <tri.nh12825@sinhvien.hoasen.edu.vn>
2023-03-11 17:31:01 +0700

    v1.1.1

--- client/src/app/pages/hometask/hometask.component.ts
+++ client/src/app/pages/hometask/hometask.component.ts
@@ -42,28 +42,28 @@ export class HometaskComponent implements OnInit{
   infoOpened: boolean = false;
 
   ngOnInit(){
-    // this.todoList = [];
-    // this.inProgressList = [];
-    // this.completeList = [];
-    // this.dueList = [];
-    // this.taskList = [];
-    // this.getAllTasks();
+    this.todoList = [];
+    this.inProgressList = [];
+    this.completeList = [];
+    this.dueList = [];
+    this.taskList = [];
+    this.getAllTasks();
   }
 
-  // getAllTasks(){
-  //   this.store.dispatch(TaskActions.getAllTasks());
-  //   this.task$.subscribe( (data: any) => {
-  //     if(data != null){
-  //       this.taskList = data.tasks;
-  //       this.todoList = this.taskList.filter((task) => task.status === 'todo');
-  //       this.inProgressList = this.taskList.filter((task) => task.status === 'in-progress');
-  //       this.completeList = this.taskList.filter((task) => task.status === 'completed');
-  //       this.dueList = this.taskList.filter((task) => task.status === 'due');
-  //     }else{
-  //       console.log('No data');
-  //     }
-  //   });
-  // }
+  getAllTasks(){
+    this.store.dispatch(TaskActions.getAllTasks());
+    this.task$.subscribe( (data: any) => {
+      if(data != null){
+        this.taskList = data.tasks;
+        this.todoList = this.taskList.filter((task) => task.status === 'todo');
+        this.inProgressList = this.taskList.filter((task) => task.status === 'in-progress');
+        this.completeList = this.taskList.filter((task) => task.status === 'completed');
+        this.dueList = this.taskList.filter((task) => task.status === 'due');
+      }else{
+        console.log('No data');
+      }
+    });
+  }
 
   taskPrj$ !: Observable<any>;
   taskPrj: TaskModel[] = [];
@@ -96,104 +96,104 @@ export class HometaskComponent implements OnInit{
     this.taskService.sendTask(task);
   }
 
-  // dialogAddTaskOpen(enterAnimationDuration: string, exitAnimationDuration: string) {
-  //   let addTaskDialog = this.matDialog.open(AddTaskComponent, {enterAnimationDuration, exitAnimationDuration, autoFocus: false});
-  //   this.prj_id = this.taskList[0].project_id;
-  //   this.chckId();
-  //   let instance = addTaskDialog.componentInstance;
-  //   instance.prj_id = this.prj_id;
-  //   instance.task_id = this.task_id;
-  // }
+  dialogAddTaskOpen(enterAnimationDuration: string, exitAnimationDuration: string) {
+    let addTaskDialog = this.matDialog.open(AddTaskComponent, {enterAnimationDuration, exitAnimationDuration, autoFocus: false});
+    this.prj_id = this.taskList[0].project_id;
+    this.chckId();
+    let instance = addTaskDialog.componentInstance;
+    instance.prj_id = this.prj_id;
+    instance.task_id = this.task_id;
+  }
 
-  // dialogTaskInfoOpen(enterAnimationDuration: string, exitAnimationDuration: string, tId: string){
-  //   let taskInfoDialog = this.matDialog.open(TaskInfoComponent, {enterAnimationDuration, exitAnimationDuration, autoFocus: false});
-  //   let instance = taskInfoDialog.componentInstance;
-  //   this.taskList.filter((task) => {
-  //     if(task.task_id === tId){ 
-  //       instance.task = task;
-  //     }
-  //   });
+  dialogTaskInfoOpen(enterAnimationDuration: string, exitAnimationDuration: string, tId: string){
+    let taskInfoDialog = this.matDialog.open(TaskInfoComponent, {enterAnimationDuration, exitAnimationDuration, autoFocus: false});
+    let instance = taskInfoDialog.componentInstance;
+    this.taskList.filter((task) => {
+      if(task.task_id === tId){ 
+        instance.task = task;
+      }
+    });
 
-  //   taskInfoDialog.afterClosed().subscribe(() => {
-  //     this.ngOnInit();
-  //   });
-  // }
+    taskInfoDialog.afterClosed().subscribe(() => {
+      this.ngOnInit();
+    });
+  }
 
-  // drop(event: CdkDragDrop<TaskModel[]>, listName: string){
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex,
-  //     );
+  drop(event: CdkDragDrop<TaskModel[]>, listName: string){
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
 
-  //     if(listName === 'todo'){
-  //       let tempList = this.updateList('todo', event.currentIndex);
-  //       this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
-  //     }else if(listName === 'in-progress'){
-  //       let tempList = this.updateList('in-progress', event.currentIndex);
-  //       this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
-  //     }else if(listName === 'completed'){
-  //       let tempList = this.updateList('completed', event.currentIndex);
-  //       this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
-  //     }else if(listName === 'due'){
-  //       let tempList = this.updateList('due', event.currentIndex);
-  //       this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
-  //     }
-  //   }
-  // }
+      if(listName === 'todo'){
+        let tempList = this.updateList('todo', event.currentIndex);
+        this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
+      }else if(listName === 'in-progress'){
+        let tempList = this.updateList('in-progress', event.currentIndex);
+        this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
+      }else if(listName === 'completed'){
+        let tempList = this.updateList('completed', event.currentIndex);
+        this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
+      }else if(listName === 'due'){
+        let tempList = this.updateList('due', event.currentIndex);
+        this.store.dispatch(TaskActions.updateTask({task: tempList, id: tempList.task_id}));
+      }
+    }
+  }
 
-  // updateList(listName: Status, index: number): TaskModel{
-  //   let list: any;
-  //   if(listName === 'todo'){
-  //     list = this.todoList;
-  //   }else if(listName === 'in-progress'){
-  //     list = this.inProgressList;
-  //   }else if(listName === 'completed'){
-  //     list = this.completeList;
-  //   }else if(listName === 'due'){
-  //     list = this.dueList;
-  //   }
+  updateList(listName: Status, index: number): TaskModel{
+    let list: any;
+    if(listName === 'todo'){
+      list = this.todoList;
+    }else if(listName === 'in-progress'){
+      list = this.inProgressList;
+    }else if(listName === 'completed'){
+      list = this.completeList;
+    }else if(listName === 'due'){
+      list = this.dueList;
+    }
 
-  //   const tmp: Mutable<TaskModel> = {
-  //     task_id: list[index].task_id,
-  //     project_id: list[index].project_id,
-  //     name: list[index].name,
-  //     description: list[index].description,
-  //     assignee: list[index].assignee,
-  //     status: listName,
-  //     complexity: list[index].complexity,
-  //     comment_count: list[index].comment_count,
-  //     deadline: list[index].deadline,
-  //     created_at: list[index].created_at,
-  //     updated_at: list[index].updated_at
-  //   };
+    const tmp: Mutable<TaskModel> = {
+      task_id: list[index].task_id,
+      project_id: list[index].project_id,
+      name: list[index].name,
+      description: list[index].description,
+      assignee: list[index].assignee,
+      status: listName,
+      complexity: list[index].complexity,
+      comment_count: list[index].comment_count,
+      deadline: list[index].deadline,
+      created_at: list[index].created_at,
+      updated_at: list[index].updated_at
+    };
 
-  //   return tmp;
-  // }
+    return tmp;
+  }
 
-  // taskIdGen(){
-  //   let id = 't';
-  //   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //   for(let i = 0; i < 10; i++){
-  //     id += possible.charAt(Math.floor(Math.random() * possible.length));
-  //   }
-  //   return id;
-  // }
+  taskIdGen(){
+    let id = 't';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for(let i = 0; i < 10; i++){
+      id += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return id;
+  }
 
-  // chckId(){
-  //   let tempID = this.taskIdGen();
-  //   for(let i =0; i < this.taskList.length; i++){
-  //     if(this.taskList[i].task_id == tempID){
-  //       console.log("id conflict");
-  //     }else{
-  //       this.task_id = tempID;
-  //     }
-  //   }
-  // }
+  chckId(){
+    let tempID = this.taskIdGen();
+    for(let i =0; i < this.taskList.length; i++){
+      if(this.taskList[i].task_id == tempID){
+        console.log("id conflict");
+      }else{
+        this.task_id = tempID;
+      }
+    }
+  }
 
   
 }
